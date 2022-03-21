c56801730b1f037bc891a2689c2894db12b9431d
Isaac Young <isaacy2012@gmail.com>
2022-03-21 20:12:49 +1300

    Add commenting

--- DiceDemo/app/src/main/java/dev/isaacy/dicedemo/MainActivity.kt
+++ DiceDemo/app/src/main/java/dev/isaacy/dicedemo/MainActivity.kt
@@ -7,21 +7,30 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * The view binding for this Activity.
+     */
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflate the view from the view binding.
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // set the text of the textview_die to "Press Roll"
         binding.textviewDie.text = getString(R.string.press_roll_die)
 
         addButtonOnClicks()
     }
 
+    /**
+     * Add button on click listeners.
+     */
     private fun addButtonOnClicks() {
         binding.buttonRollDie.setOnClickListener {
+            // set the text of the textview_die to a random number from 1 to dieRange inclusive
             binding.textviewDie.text = Random.nextInt(1, 7).toString()
         }
     }
