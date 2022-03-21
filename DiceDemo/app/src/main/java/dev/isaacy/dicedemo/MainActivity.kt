5a87b1d23191754cbc625081c968e6021396aeff
Isaac Young <isaacy2012@gmail.com>
2022-03-21 19:39:35 +1300

    Simple six sided die

--- DiceDemo/app/src/main/java/dev/isaacy/dicedemo/MainActivity.kt
+++ DiceDemo/app/src/main/java/dev/isaacy/dicedemo/MainActivity.kt
@@ -2,10 +2,27 @@ package dev.isaacy.dicedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.isaacy.dicedemo.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textviewDie.text = getString(R.string.press_roll_die)
+
+        addButtonOnClicks()
+    }
+
+    private fun addButtonOnClicks() {
+        binding.buttonRollDie.setOnClickListener {
+            binding.textviewDie.text = Random.nextInt(1, 7).toString()
+        }
     }
 }
